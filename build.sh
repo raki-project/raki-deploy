df3cf671bb016b427d7369c337f2d1007ddb3149
Rene Speck <speckrene@gmail.com>
2021-05-05 07:36:30 +0200

    Update.

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ cd build
 export KEY1_PATH=./raki-verbalizer/deployKey
 export KEY2_PATH=./raki-verbalizer-pipeline/deployKey
 export KEY3_PATH=./raki-verbalizer-webapp/deployKey
+
+export SSH_PRIVATE_KEY=<Add your key here>
+
 ./dockerBuild.sh
--- build/dockerBuild.sh
+++ build/dockerBuild.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+key_content0=$(cat $SSH_PRIVATE_KEY)
 key_content1=$(cat $KEY1_PATH)
 key_content2=$(cat $KEY2_PATH)
 key_content3=$(cat $KEY3_PATH)
+
 docker build \
          --progress=plain \
          --rm -t raki-verbalizer-webapp \
+         --build-arg \
+         SSH_PRIVATE_KEY="${key_content0}" \
          --build-arg \
          KEY_VERBALIZER="${key_content1}" \
          --build-arg \
